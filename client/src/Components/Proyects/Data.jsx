fe8754d75510d53213a42d18ee8f8ee64f4a9a41
sznitowski <vsznitowski@gmail.com>
2021-12-10 13:30:02 -0300

    deploy commit

--- client/src/Components/Proyects/Data.jsx
+++ client/src/Components/Proyects/Data.jsx
@@ -1,8 +1,8 @@
 import video1 from '../../assets/proyects/video1.mp4';
 import video2 from '../../assets/proyects/video2.mp4';
 
-export const url1 = 'https://github.com/sznitowski/Agenda-React';
-export const url2 = 'https://github.com/sznitowski';
+export const url1 = 'https://github.com/sznitowski/moneyNote';
+export const url2 = 'https://github.com/sznitowski/';
 
 export const description1 = "Es un Challenge para poder participar en Alkemy, es aplicacion donde podes guardar y tener un balance de gastos, para su desarrollo utilice React, Axios, Nodejs, Express, Sequelize, PostgreSql, tambien esta deployada en Amazon AWS";
 export const description2 = "App trabajada para Alkemy la cual se desarrollo durante 1 mes y medio, la trabajamos entre 5 personas, utilizando metodologias Agilies Jira, Bitbucket para alojar el Codigo, git, retroteam, es un proyecto fullstack con React-Redux, Reactstrap, Reacslick, Sass, Axios, Nodejs, Express, Sequelize, mocha-chai, mysql...";
@@ -10,16 +10,16 @@ export const description2 = "App trabajada para Alkemy la cual se desarrollo dur
 export default [
   {
     id: 1,
-    'title': 'Ong-App',
+    'title': 'Alkemy-Challenge',
     'subtitle': 'React, Nodejs, express, Postgresql',
     video: video1,
     url: url1,
     description: description1
   },
   {
-    id: 1,
-    'title': 'Alkemy-Challenge',
+    'title': 'Ong-App',
     'subtitle': 'React, redux, NodeJs, Sequelize, Mysql',
+    id: 2,
     video: video2,
     url: url2,
     description: description2
